a2bf4d1910a7ea25ef3d252c47cbb196b9bbc4c9
Flinn G <flinnja@gmail.com>
2019-09-14 23:53:45 +1200

    add validations to pokemon model

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,6 @@
 class Pokemon < ApplicationRecord
+	validates :name, presence: {message: "Name cannot be empty"}, uniqueness: {case_sensitive: false, message: "Name already exists in database"}
+
+	validates :national_dex_no, presence: {message: "Dex Number cannot be empty"}, uniqueness: {case_sensitive: false, message: "Dex number already exists in database"}
+
 end
