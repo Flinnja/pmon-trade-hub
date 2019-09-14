f2c19b5670f7948b3fa32105b3313f680d4a877f
Flinn G <flinnja@gmail.com>
2019-09-15 00:00:16 +1200

    capitalize all pokemon names before validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -3,4 +3,5 @@ class Pokemon < ApplicationRecord
 
 	validates :national_dex_no, presence: {message: "Dex Number cannot be empty"}, uniqueness: {case_sensitive: false, message: "Dex number already exists in database"}
 
+	before_validation name.capitalize if name.present?
 end
